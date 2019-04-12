de6ec9ba3bc15e244545787e6e83e5efe8409f02
edwardgnt <edwardgnt@gmail.com>
2019-04-11 22:00:57 -0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['prefix' => 'laravel-ajax-modal'], function () {
+    Route::get('/', 'Crud5Controller@index');
+    Route::match(['get', 'post'], 'create', 'Crud5Controller@create');
+    Route::match(['get', 'put'], 'update/{id}', 'Crud5Controller@update');
+    Route::delete('delete/{id}', 'Crud5Controller@delete');
+});
